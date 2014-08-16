0ad97adbbf28cb108510ad58ec93e36eaf94a50d
JacquesMurray <jmmurray@gabric.info>
2014-08-16 13:17:13 +0200

    Created Database Model Library
    Created portable C# library called Gabric.ZAPostalCodes.DatabaseModel
    
    Created Suburb and Area classed

---
+++ ZA Postal Codes/Gabric.ZAPostalCodes.DatabaseModel/Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gabric.ZAPostalCodes.DatabaseModel
+{
+    public class Area
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Suburb> AreaSuburbs { get; set; }
+    }
+}
---
+++ ZA Postal Codes/Gabric.ZAPostalCodes.DatabaseModel/Suburb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gabric.ZAPostalCodes.DatabaseModel
+{
+    public class Suburb
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BoxCode { get; set; }
+        public string StreetCode { get; set; }
+        public Area SuburbArea { get; set; }
+    }
+}
