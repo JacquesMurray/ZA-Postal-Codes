15355ea02981ec49f78ea9b7478409a92847e33c
JacquesMurray <jmmurray@gabric.info>
2014-08-16 13:43:56 +0200

    Created WCF Service and recreated data models

--- ZA Postal Codes/Gabric.ZAPostalCodes.ZAPostalCodesWCFService/Properties/AssemblyInfo.cs
+++ ZA Postal Codes/Gabric.ZAPostalCodes.ZAPostalCodesWCFService/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gabric.ZAPostalCodes.DatabaseModel")]
+[assembly: AssemblyTitle("Gabric.ZAPostalCodes.ZAPostalCodesWCFService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gabric.ZAPostalCodes.DatabaseModel")]
+[assembly: AssemblyProduct("Gabric.ZAPostalCodes.ZAPostalCodesWCFService")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96fbef86-b0ea-49d1-9ca9-6c10008896e0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -23,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
